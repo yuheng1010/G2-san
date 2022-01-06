d319c145f06795d767cce1ec8b1c3475a0bf22d0
vivi <109306017@nccu.edu.tw>
2022-01-06 23:36:16 +0800

    0106

---
+++ SanWeb0106/src/WordCounter.java
@@ -0,0 +1,66 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+
+import javax.net.ssl.SSLHandshakeException;
+
+public class WordCounter {
+ private String urlStr;
+    private String content;
+    
+    public WordCounter(String urlStr){
+     urlStr = urlStr.substring(7);
+     this.urlStr = urlStr;
+    }
+    
+    private String fetchContent() throws IOException,SSLHandshakeException{
+    	try {
+			  URL url = new URL(this.urlStr);
+			  URLConnection conn = url.openConnection();
+			  InputStream in = conn.getInputStream();
+			  BufferedReader br = new BufferedReader(new InputStreamReader(in,"UTF-8"));
+			 
+			  String retVal = "";
+			 
+			  String line = null;
+			  
+			  while ((line = br.readLine()) != null){
+			      retVal = retVal + line + "\n";
+		  }
+		 
+		  return retVal;
+      }
+      catch(SSLHandshakeException e1)
+      {
+       return "a";
+      }
+      catch(IOException e)
+      {
+       return "b";
+      }
+    }
+    
+    public int countKeyword(String keyword) throws IOException{
+  if (content == null){
+      content = fetchContent();
+  }
+  //To do a case-insensitive search, we turn the whole content and keyword into upper-case:
+  content = content.toUpperCase();
+  keyword = keyword.toUpperCase();
+ 
+  int retVal = 0;
+  int fromIdx = 0;
+  int found = -1;
+ 
+  while ((found = content.indexOf(keyword, fromIdx)) != -1){
+      retVal++;
+      fromIdx = found + keyword.length();
+  }
+     //System.out.print(retVal);
+  return retVal;
+    }
+}
